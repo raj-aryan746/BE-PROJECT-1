caa73c2a13ed0d6a3e9a8e567749c7b4ec0943ec
Raj Aryan <rajaryansingh09012004@gmail.com>
2025-11-28 19:36:25 +0530

    backend end is set up

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config(
+    { 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    }
+);
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return console.log("file upload FAILED"); 
+         
+        const response = await cloudinary.uploader.upload( localFilePath, { resource_type: "auto" } );    //upload the file on cloudinary
+        
+        console.log("file is uploaded successfull", response.url);          // file has been uploaded successfull
+
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath);     // remove the locally saved file as the upload opration got failed
+
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
